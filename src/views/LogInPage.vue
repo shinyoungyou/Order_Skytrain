c5606170c90d6fe46730d0fc14240bf35922c514
AustynYou <austyn.you@gmail.com>
2022-11-11 19:15:02 -0800

    🚧 Implement product list render

---
+++ src/views/LogInPage.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <input v-model="uname" placeholder="Username" />
+    <input v-model="pass" placeholder="Password" type="password" />
+    <input type="checkbox" id="remember"/>Remember me
+    <button v-on:click="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data(){
+    return {
+      uname: "",
+      pass: ""
+    }
+    
+  },
+  methods: {
+    login(){
+      fetch('./data/customers.json').then(
+            (data)=>{
+                data.json().then((users)=>{
+                    users.forEach(()=>{
+                        // if(user.userName== uname && user.password==$scope.pass){
+                        //     $rootScope.logFlag = true;
+                        //     logedUser = new userClass(user.customerId,user.userName,user.first_name,user.last_name,user.email);
+                        //     $rootScope.logedUser = logedUser;
+                        //     $rootScope.shopCart = new shoppingCart(logedUser.toObject().cid);
+                        //     $location.path('/products');
+                        // }
+                    })
+                })
+            }
+        )
+    }
+
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/SandwichPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <article class="hello">
+    <aside v-for="sandwich in products.sandwiches" :key="sandwich.pid">
+        <img :src="sandwich.listImg" :alt="sandwich.pName">
+        <h3>{{sandwich.pName}}</h3>
+    </aside>
+  </article>
+</template>
+
+<script>
+import productList from '../data/products-data.json'
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data(){
+    return {
+      products: productList
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+article {
+  display: flex;
+  flex-wrap: wrap;
+
+}
+</style>
