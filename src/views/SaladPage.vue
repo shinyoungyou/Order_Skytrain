7d8e568fe304aa6926fb53eccda36ad592e3fad2
AustynYou <austyn.you@gmail.com>
2022-11-11 19:14:16 -0800

    🚧 Implement product list render

---
+++ src/views/SaladPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <article class="hello">
+    <aside v-for="salad in products.salads" :key="salad.pid">
+        <img :src="salad.listImg" :alt="salad.pName">
+        <h3>{{salad.pName}}</h3>
+    </aside>
+  </article>
+</template>
+
+<script>
+import productList from '../data/products-data.json'
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data(){
+    return {
+      products: productList
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+article {
+  display: flex;
+  flex-wrap: wrap;
+
+}
+</style>
