69f401379bf91e5c4a176a34006a4c20508e3d5a
jiwonlee0120 <jiwonlee0120@gmail.com>
2022-11-18 20:11:30 -0800

    Update App.vue
    changed font.

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,9 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Lato', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
