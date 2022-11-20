c34f36548558fadb0902b140914191b5afb320d1
jiwonlee0120 <jiwonlee0120@gmail.com>
2022-11-19 22:31:37 -0800

    giva a color to the current page
    give a color to the current page

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://10.2.121.178:8084/",
+  baseURL: "http://192.168.0.30:8087/",
   headers: { "content-type": "application.json" },
 });
