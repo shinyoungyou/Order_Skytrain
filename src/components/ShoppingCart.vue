42a89c577a446b53301e29f3274f2c242f5510f1
roksslee <111397057+roksslee@users.noreply.github.com>
2022-11-22 13:05:45 -0800

    Working on cart

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -4,28 +4,34 @@
             <tbody>
                 <tr v-for="item in selectItems" :key="item.iid">
                     <td>{{item.iName}}</td>
-                    <td>{{item.price}}</td>
+                    <td v-if="!cartFlag">{{item.price}}</td>
                     <td>{{item.cals}}</td>
                     <td>{{item.amount}}</td>
                 </tr>
             </tbody>
             <tfoot>
                 <tr>
-                    <td><button>-</button>{{}}<button>+</button></td>
+                    <td><button @click="flagCtrl">-</button>{{total}}<button>+</button></td>
                     <td>{{total}}</td>
                 </tr>
             </tfoot>
         </table>
-        <table>
+        <table v-if="!cartFlag">
             <thead>
                 <tr>
                     <th>Products</th>
                     <th>Amount</th>
                     <th>Price</th>
+                    <th>Calories</th>
                 </tr>
             </thead>
             <tbody>
-                
+                <tr v-for="(product, iid) in products" :key="iid">
+                    <td>{{product}}</td>
+                    <td>{{product.amount}}</td>
+                    <td>{{product.total()}}</td>
+                    <td>{{product.calTotal()}}</td>
+                </tr>
             </tbody>
         </table>
     </section>
@@ -39,13 +45,30 @@ export default {
     ],
     data(){
         return {
-            products: null
+            products: new Map(),
+            shopFlag: false
         }
     },
     methods: {
         addCart(){
-            this.products = new ProductClass(this.selectItems.iid, this.selectItems.iName, this.selectItems.cals, this.selectItems.price);
+            let selProd = null;
+            if(this.products.has(this.selectItems.iid)){
+                selProd = this.products.get(this.selectItems.iid);
+                selProd.amount = selProd.amount + 1;
+            }else{
+                selProd = new ProductClass(this.selectItems.iid, this.selectItems.iName, this.selectItems.cals, this.selectItems.price);
+            }
+            this.products.set(selProd.iid, selProd);
+        },
+        flagCtrl(){
+            return this.cartFlag = true;
         }
+    },
+    mounted(){
+        
     }
 }
 </script>
+<style scoped>
+
+</style>
